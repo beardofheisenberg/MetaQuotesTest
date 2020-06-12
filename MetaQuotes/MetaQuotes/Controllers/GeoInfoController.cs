1d66943191c8b6912252adc3ff41b9bc8b88fe4e
aorlovskiy <aorlovskiy90@gmail.com>
2020-06-12 17:48:37 +0300

    Prepared the application

---
+++ MetaQuotes/MetaQuotes/Controllers/GeoInfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetaQuotes.Controllers
+{
+    [ApiController]
+    public class GeoInfoController : ControllerBase
+    {
+        [HttpGet("/ip/location")]
+        public async Task<string> GetLocationByIp([FromQuery] string ip)
+        {
+            return await Task.FromResult($"I've received {ip}");
+        }
+
+        [HttpGet("/city/locations")]
+        public async Task<string> GetLocationsByCity([FromQuery] string city)
+        {
+            return await Task.FromResult($"I've received {city}");
+        }
+    }
+}
---
+++ MetaQuotes/MetaQuotes/wwwroot/js/root.js
@@ -0,0 +1,63 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    //add event handlers for menu items
+    [].forEach.call(document.querySelectorAll('.container ul li'), function (element) {
+        element.addEventListener('click', function () {
+            //switch menu item
+            var allChildItems = this.parentElement.getElementsByTagName('li');
+
+            for (var i = 0; i < allChildItems.length; i++)
+                allChildItems[i].classList.remove('active');
+
+            this.classList.add('active');
+
+            //switch content
+            var relatedElementId = this.getAttribute('data-related-content');
+            var relatedElement = document.getElementById(relatedElementId);
+            var allChildContentItems = relatedElement.parentElement.getElementsByTagName('div');
+
+            for (var i = 0; i < allChildContentItems.length; i++)
+                allChildContentItems[i].classList.remove('active');
+
+            relatedElement.classList.add('active');
+        })
+    });
+
+    //add event handlers for forms
+    Array.prototype.forEach.call(document.getElementsByTagName('form'), function (element) {
+        element.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            var paramName = this.getAttribute('data-param-name');
+            var paramValue = this.getElementsByTagName('input')[0].value;
+            var url = `${this.getAttribute('action')}?${paramName}=${paramValue}`;
+
+            ajax(url,
+                function (result) {
+                    alert(result);
+                },
+                function (result) {
+                    alert(result);
+                });
+        });
+    });
+});
+
+function ajax(url, onSuccess, onError) {
+    var requester = new XMLHttpRequest();
+
+    requester.addEventListener('readystatechange', function () {
+        if (this.readyState == 4) {
+            var result = this.responseText;
+
+            if (this.status == 200) {
+                onSuccess(result);
+            } else {
+                onError(result);
+            }
+        }
+    });
+
+    requester.open('get', url, true);
+    requester.send();
+}
