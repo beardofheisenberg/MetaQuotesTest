2389b35d94506e70dbb2b6932af7970a6b50da85
aorlovskiy <aorlovskiy90@gmail.com>
2020-06-15 23:22:19 +0300

    Temp

--- MetaQuotes/MetaQuotes/Controllers/GeoInfoController.cs
+++ MetaQuotes/MetaQuotes/Controllers/GeoInfoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MetaQuotes.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +8,45 @@ using System.Threading.Tasks;
 namespace MetaQuotes.Controllers
 {
     [ApiController]
+    [Produces("application/json")]
     public class GeoInfoController : ControllerBase
     {
         [HttpGet("/ip/location")]
-        public async Task<string> GetLocationByIp([FromQuery] string ip)
-        {
-            return await Task.FromResult($"I've received {ip}");
-        }
+        public async Task<Location> GetLocationByIp([FromQuery] string ip)
+            => new Location
+            {
+                Country = $"cou_{ip}",
+                Region = $"reg_{ip}",
+                Postal = $"pos_{ip}",
+                City = $"cit_{ip}",
+                Organization = $"org_{ip}",
+                Latitude = 55.2321321F,
+                Longitude = 37.4687322F
+            };
 
         [HttpGet("/city/locations")]
-        public async Task<string> GetLocationsByCity([FromQuery] string city)
-        {
-            return await Task.FromResult($"I've received {city}");
-        }
+        public async Task<IEnumerable<Location>> GetLocationsByCity([FromQuery] string city)
+            => new List<Location> {
+                new Location
+                {
+                    Country = $"cou_{city}",
+                    Region = $"reg_{city}",
+                    Postal = $"pos_{city}",
+                    City = $"{city}",
+                    Organization = $"org_{city}",
+                    Latitude = 55.2321321F,
+                    Longitude = 37.4687322F
+                },
+                 new Location
+                {
+                    Country = $"cou_{city}",
+                    Region = $"reg_{city}",
+                    Postal = $"pos_{city}",
+                    City = $"{city}",
+                    Organization = $"org_{city}",
+                    Latitude = 55.2321321F,
+                    Longitude = 37.4687322F
+                }
+            };
     }
 }
